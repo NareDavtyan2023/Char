7757ad5ba35d7bcab8487a9d53f01e89a77a20b1
Admin <Admin@DESKTOP-A06UMIO>
2023-06-12 23:35:49 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CHaaar
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string text;
+            //Console.Write("Input text: ");
+            //text = Console.ReadLine();
+  
+            Console.Write("Enter a string :");
+            string str = Console.ReadLine();
+
+            int totalCharacter = 0, totalLetterChar = 0;
+
+            char[] strArray = str.ToCharArray();
+            foreach (var item in strArray)
+            {
+                if (char.IsLetter(item))
+                    totalLetterChar++;
+              
+            }
+
+            
+            //Console.WriteLine("Entered String : " + str);
+           
+            Console.WriteLine("Total Letter Character String : " + totalLetterChar);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
